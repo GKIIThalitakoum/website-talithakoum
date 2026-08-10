43db63c3aa364609d8c81c1bdc9e0bec7fc79ed5
GKIIThalitakoum <gkiithalitakoum@gmail.com>
2026-08-10 23:25:14 +0800

    Tambah config.js

---
+++ src/lib/config.js
@@ -0,0 +1,3 @@
+export const NAMA_GEREJA = "GKII Jemaat Talitha Koum Sendawar";
+export const ALAMAT = "Jl. Mangku Aji RT. 7 Kamp. Sumber Bangun, Kec. Sekolaq Darat, Kab. Kutai Barat, Kalimantan Timur";
+export const URL_BULETIN = "https://buletin.talithakoum.my.id";
